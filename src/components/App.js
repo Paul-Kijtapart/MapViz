8cd89494bfa560eab279b696754def8ed8117912
Paul-Kijtapart <scc385@gmail.com>
2017-02-13 00:10:16 -0800

    remove unused json, ensured all data are being used to locate some areas on the  map.

--- src/components/App.js
+++ src/components/App.js
@@ -23,7 +23,7 @@ import UNZONED from 'maps/nw/UNZONED.json';
 
 class App extends React.Component {
 
-	style2_2(feature) {
+	style_2(feature) {
 		var color = '#F0' + feature.properties.Name.length % 9 + 'F26';
 		console.log(feature)
 		return {
@@ -40,51 +40,63 @@ class App extends React.Component {
 		const mapboxAccessToken = 'pk.eyJ1IjoibmFwb24iLCJhIjoiY2l6MzdneThwMDUwbjJ3bjE0a2QxanB1NyJ9.2zlCnkvfXLp5AAfoMbeQSQ';
 		const position = [49.207, -122.912];
 		const map = (
-			<Map id="main_map" center={position} zoom={13}>
-				<TileLayer
-      				url={'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken}
-   					id="mapbox.light"
-   				/>
-   				<GeoJSON 
-   					data={AGRICULTURAL}
-   					style={this.style2_2}
-   				/>
-   				<GeoJSON 
-   					data={APARTMENT_HIGH}
-   					style={this.style2_2}
-   				/>
-   				<GeoJSON 
-   					data={APARTMENT_LOW}
-   					style={this.style2_2}
-   				/>
-   				<GeoJSON 
-   					data={COMMERCIAL}
-   					style={this.style2_2}
-   				/>
-   				<GeoJSON 
-   					data={DUPLEX}
-   					style={this.style2_2}
-   				/>
-   				<GeoJSON 
-   					data={INDUSTRIAL}
-   					style={this.style2_2}
-   				/>
-   				<GeoJSON 
-   					data={MIXED}
-   					style={this.style2_2}
-   				/>
-   				<GeoJSON 
-   					data={SINGLE}
-   					style={this.style2_2}
-   				/>
-   				<GeoJSON 
-   					data={TOWNHOUSE}
-   					style={this.style2_2}
-   				/>
-   				<GeoJSON 
-   					data={UNZONED}
-   					style={this.style2_2}
-   				/>
+			<Map id="main_map" 
+				center={position} 
+				zoom={13}
+				zoomControl={false}
+				touchZoom={false}
+				doubleClickZoom={false}
+				scrollWheelZoom={false}
+				boxZoom={false}
+				dragging={false} >
+					<TileLayer
+	      				url={'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken}
+	   					id="mapbox.light"
+	   				/>
+	   				<GeoJSON 
+	   					data={AGRICULTURAL}
+	   					style={this.style_2}
+	   				/>
+	   				<GeoJSON 
+	   					data={APARTMENT_HIGH}
+	   					style={this.style_2}
+	   				/>
+	   				<GeoJSON 
+	   					data={APARTMENT_LOW}
+	   					style={this.style_2}
+	   				/>
+	   				<GeoJSON 
+	   					data={COMMERCIAL}
+	   					style={this.style_2}
+	   				/>
+	   				<GeoJSON 
+	   					data={DUPLEX}
+	   					style={this.style_2}
+	   				/>
+	   				<GeoJSON 
+	   					data={INDUSTRIAL}
+	   					style={this.style_2}
+	   				/>
+	   				<GeoJSON 
+	   					data={INSTITUTIONAL}
+	   					style={this.style_2}
+	   				/>
+	   				<GeoJSON 
+	   					data={MIXED}
+	   					style={this.style_2}
+	   				/>
+	   				<GeoJSON 
+	   					data={SINGLE}
+	   					style={this.style_2}
+	   				/>
+	   				<GeoJSON 
+	   					data={TOWNHOUSE}
+	   					style={this.style_2}
+	   				/>
+	   				<GeoJSON 
+	   					data={UNZONED}
+	   					style={this.style_2}
+	   				/>
 			</Map>
 		);
 		return (
--- src/index.js
+++ src/index.js
@@ -4,11 +4,7 @@ import ReactDOM from 'react-dom';
 // components
 import App from './components/App.js';
 
-// Json Data
-import map_locations from 'maps/map.json';
-
-
 ReactDOM.render(
-	<App map_locations={map_locations}/>,
+	<App />,
 	document.getElementById('root')
 );
