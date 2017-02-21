9892501778ecf6ca32e0aae4b10d4828ef22e038
Napon Taratan <napon@napontaratan.com>
2017-02-21 15:47:37 -0800

    Added db models

--- server/mysite/polls/models.py
+++ server/mysite/polls/models.py
@@ -3,3 +3,31 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Zone(models.Model):
+    name = models.IntegerField(primary_key=True)
+    zone_type = models.CharField(max_length=30)
+
+class Coordinate(models.Model):
+    name = models.ForeignKey(Zone, on_delete=models.CASCADE)
+    lat = models.FloatField()
+    lon = models.FloatField()
+
+class Score(models.Model):
+    name = models.IntegerField()
+    year = models.IntegerField()
+    score = models.IntegerField()
+
+
+class Incident(models.Model):
+    name = models.CharField(max_length=30)
+    lat = models.FloatField()
+    lon = models.FloatField()
+    year = models.IntegerField()
+    count = models.IntegerField()
+    zone_name = models.ForeignKey(Zone, on_delete=models.CASCADE)
+
+class Institution(models.Model):
+    name = models.CharField(max_length=30)
+    lat = models.FloatField()
+    lon = models.FloatField()
+    zone_name = models.ForeignKey(Zone, on_delete=models.CASCADE)
