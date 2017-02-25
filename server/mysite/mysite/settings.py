0f9b7b475750ee8a8d8292f167e2686d03c50206
Paul-Kijtapart <scc385@gmail.com>
2017-02-24 19:56:36 -0800

    Server is not running on Paul mac

--- server/mysite/mysite/settings.py
+++ server/mysite/mysite/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 import os
-from secret_config import *
+from mysite.secret_config import DATABASES
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
