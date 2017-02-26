a342979fb30b6adf085d5b10d0526ba4c407b8b9
Napon Taratan <napon@napontaratan.com>
2017-02-25 16:35:02 -0800

    Progress so far on load score

---
+++ server/mysite/polls/management/commands/load_score_model.py
@@ -0,0 +1,39 @@
+from django.core.management.base import BaseCommand, CommandError
+import json
+from os import path
+from os import listdir
+import mysite
+from pprint import pprint
+from GeoUtils import *
+
+# Models
+from polls.models import Incident
+from polls.models import Coordinate
+from polls.models import Institution
+from polls.models import Zone
+from polls.models import Score
+
+class Command(BaseCommand):
+  help = 'Calculate score Table'
+
+  def handle(self, *args, **options):
+    zones = Zone.objects.all()
+    for z in zones:
+      score = 0
+      institutions = Institution.objects.all()
+      for ins in institutions:
+        score = score + 1 #TODO: dan function here
+
+      incidents = Incident.objects.filter(zone_name=z.name).order_by('year')
+      s = None
+      y = None
+      for inc in incidents:
+        crash_count = 1 if int(inc.count) == 0 else inc.count
+        s = Score()
+        s.score = score
+        y = int(inc.year)
+        s.year = y
+        s.name = z.name
+        s.score = int(s.score) + (1/float(crash_count))
+        pprint(s)
+
