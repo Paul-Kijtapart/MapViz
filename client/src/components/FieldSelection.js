f10a11cb88339f4e6311066aca4cc181d8989ad7
Paul-Kijtapart <scc385@gmail.com>
2017-02-25 22:05:29 -0800

    range slider is now working

--- client/src/components/FieldSelection.js
+++ client/src/components/FieldSelection.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import Slider from 'react-rangeslider';
+// To include the default styles
+import 'react-rangeslider/lib/index.css'
 
 class FieldSelection extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: 25,
+			reverseValue: 8
+		}
+
+		this.handleChangeReverse = this.handleChangeReverse.bind(this);
+	}
+
+	handleChangeReverse(value) {
+		this.setState({
+			reverseValue: value
+		});
+	}
+
 	render() {
-		const Years = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017];
+		const {
+			value,
+			reverseValue
+		} = this.state;
+
 		return (
 			<div className="fieldSelection">
+				<div className='slider-horizontal'>
+					<Slider
+		              min={0}
+		              max={100}
+		              step={10}
+		              value={reverseValue}
+		              orientation='horizontal'
+		              onChange={this.handleChangeReverse}
+					/>
+					<div className='value'>{reverseValue}</div>
+				</div>
 				<h1> I am fieldSelection. </h1>
 			</div>
 		);
